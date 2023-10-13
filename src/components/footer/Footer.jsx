b9ceb84cb592f7232963cd97f585bfc6d09d4213
Sean Adams <seanadams.adams@gmail.com>
2023-10-13 10:15:57 +0100

    Configure React app for deployment to GitHub Pages

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Sean Adams</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#skills" className="footer__link">
+              Skills
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" className="footer__link">
+              Contact
+            </a>
+          </li>
+{/* 
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li> */}
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://github.com/SeanAdamsDev"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-github"></i>
+          </a>
+
+
+          <a
+            href="https://www.linkedin.com/in/sean-adams-1b0440127/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i className="bx bxl-linkedin"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; SeanAdams. All rigths reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/sean-adams-1b0440127/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/SeanAdamsDev"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
